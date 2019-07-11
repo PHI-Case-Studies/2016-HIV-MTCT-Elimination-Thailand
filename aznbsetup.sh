a3fe1adcce52472754eaacf1e5edd04937d5a261
Herman Tolentino <herman.tolentino@gmail.com>
2019-07-11 18:43:44 -0500

    updated set up files

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,11 +5,13 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* pandas=0.24.2 json numpy matplotlib=3.0.3 geopandas=0.5.0
-conda install -y -c conda-forge pysal=2.0.0 seaborn=0.9.0 fuzzywuzzy=0.17.0 
+conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* 
+conda install -y numpy matplotlib=3.0.3 geopandas=0.5.0
+conda install -y -c conda-forge pysal seaborn=0.9.0 fuzzywuzzy
 conda install -y -c plotly plotly_express
 
 pip install --upgrade pip
+pip install --yes json pandas==0.24.2
 
 git clone https://github.com/BoseCorp/py-googletrans.git
 cd /home/nbuser/py-googletrans
