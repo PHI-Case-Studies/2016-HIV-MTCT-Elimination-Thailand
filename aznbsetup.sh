237b85ea8ef1c34cfba4575f0f0be2aa8df61a96
nravilla <nirupakanthi@gmail.com>
2025-05-20 19:20:11 -0400

    package upgrades

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -8,19 +8,30 @@ conda activate base
 
 # Install packages with mamba
 mamba install -y -c conda-forge \
+    git \
+    ipywidgets \
+    ipython \
+    ipykernel \
+    jupyterlab \
+    nodejs \
     folium=0.14.0 \
     jinja2=3.1.2 \
     numpy \
+    sqlite \
     matplotlib=3.7 \
     geopandas=0.13 \
     pandas=2.2 \
     seaborn \
+    fiona \
+    pyproj \
     fuzzywuzzy \
-    plotly
+    plotly \
+    pip \
+    pysal
 
-# Upgrade pip and install py-googletrans directly from GitHub
-pip install --upgrade pip
-pip install git+https://github.com/BoseCorp/py-googletrans.git
+# Install googletrans fork explicitly
+/opt/conda/bin/pip install googletrans
 
 # Cleanup
 conda clean --all --yes
+
